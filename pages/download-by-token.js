90e69d4a5dd7e07b7d695745d8cc71f228f5046c
Aminejvm <elouartinra@hotmail.com>
2021-03-05 19:11:27 +0100

    multi-download endpoints

---
+++ pages/download-by-token.js
@@ -0,0 +1,62 @@
+import archiver from "archiver";
+import Request from "request";
+import Redis from "ioredis";
+
+const redisClient = new Redis({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD,
+});
+
+const request = (link) => Request.get(link);
+
+const DOWNLOAD_ERROR_MESSAGE = `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<script>
+          parent.postMessage('SLATE_DOWNLOAD_ERROR','${process.env.RESOURCE_URI_SLATE}');
+    </script>
+	</head>
+	<body></body>
+</html>`;
+
+export default async (req, res) => {
+  const downloadId = req.query.downloadId;
+  const fileName = req.query.name;
+
+  let files = [];
+  try {
+    files = JSON.parse(await redisClient.get(downloadId));
+    if (!files) throw new Error("invalid files");
+  } catch (e) {
+    console.log(e);
+    return res.status(400).header("Content-Type", "text/html").send(DOWNLOAD_ERROR_MESSAGE);
+  }
+
+  const archive = archiver("zip");
+
+  try {
+    archive.on("warning", function (err) {
+      if (err.code === "ENOENT") {
+        console.log("download warning", err);
+      } else {
+        // throw error
+        throw err;
+      }
+    });
+
+    archive.on("error", function (err) {
+      throw err;
+    });
+
+    res.attachment(fileName);
+    archive.pipe(res);
+    files.forEach((file) => archive.append(request(file.url), { name: file.name }));
+    archive.finalize();
+    redisClient.del(downloadId);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).header("Content-Type", "text/html").send(DOWNLOAD_ERROR_MESSAGE);
+  }
+};
