397993d2028cbd1f75882b1def3d26c785d9eda5
martinalong <33686587+martinalong@users.noreply.github.com>
2021-08-10 19:34:50 -0700

    Revert "Update: env variables"

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,26 @@
 import * as Environment from "~/node_common/environment";
 
-const developmentConfig = {
-  client: "pg",
-  connection: {
-    ssl: true,
-    port: 5432,
-    host: Environment.POSTGRES_HOSTNAME,
-    database: Environment.POSTGRES_DATABASE,
-    user: Environment.POSTGRES_ADMIN_USERNAME,
-    password: Environment.POSTGRES_ADMIN_PASSWORD,
-  },
-};
-
-const productionConfig = {
-  client: "pg",
-  connection: {
-    ssl: true,
-    port: 5432,
-    host: Environment.POSTGRES_HOSTNAME,
-    database: Environment.POSTGRES_DATABASE,
-    user: Environment.POSTGRES_ADMIN_USERNAME,
-    password: Environment.POSTGRES_ADMIN_PASSWORD,
-  },
-};
-
 module.exports = {
-  development: developmentConfig,
-  staging: productionConfig,
-  production: productionConfig,
+  development: {
+    client: "pg",
+    connection: {
+      ssl: true,
+      port: 5432,
+      host: Environment.POSTGRES_HOSTNAME,
+      database: Environment.POSTGRES_DATABASE,
+      user: Environment.POSTGRES_ADMIN_USERNAME,
+      password: Environment.POSTGRES_ADMIN_PASSWORD,
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      ssl: true,
+      port: 5432,
+      host: Environment.POSTGRES_HOSTNAME,
+      database: Environment.POSTGRES_DATABASE,
+      user: Environment.POSTGRES_ADMIN_USERNAME,
+      password: Environment.POSTGRES_ADMIN_PASSWORD,
+    },
+  },
 };
