8974058e267bd011ae850c54015c891b0b4150ea
Martina <martinalong10@gmail.com>
2021-08-12 13:07:23 -0700

    missing node_env variable

--- node_common/database.js
+++ node_common/database.js
@@ -3,7 +3,7 @@ import * as Environment from "~/node_common/environment";
 import configs from "~/knexfile";
 import knex from "knex";
 
-const envConfig = configs[Environment.NODE];
+const envConfig = configs[Environment.NODE_ENV];
 const Database = knex(envConfig);
 
 export default Database;
